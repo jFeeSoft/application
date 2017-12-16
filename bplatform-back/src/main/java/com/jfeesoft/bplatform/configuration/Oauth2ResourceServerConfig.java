8c1b6e6c63e3ff3ed9b41f0897f19c3f71fee57a
twojcicki <tomasz.wojcicki@wp.pl>
2017-12-16 22:56:20 +0100

    angular - maven build

--- bplatform-back/src/main/java/com/jfeesoft/bplatform/configuration/Oauth2ResourceServerConfig.java
+++ bplatform-back/src/main/java/com/jfeesoft/bplatform/configuration/Oauth2ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.jfeesoft.bplatform.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -12,6 +13,12 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.requestMatchers().antMatchers("/permission/*").and().authorizeRequests().anyRequest().authenticated();
+		http.anonymous().and().authorizeRequests()
+				.antMatchers("/", "/lib/*", "/images/*", "/css/*", "/swagger-ui.js", "/swagger-ui.min.js", "/api-docs",
+						"/fonts/*", "/api-docs/*", "/api-docs/default/*", "/o2c.html", "index.html", "/webjars/**",
+						"/hystrix/**")
+				.permitAll().antMatchers(HttpMethod.GET, "/my").access("#oauth2.hasScope('my-resource.read')")
+				.anyRequest().authenticated();
 
 	}
 
